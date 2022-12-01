ac093d57f261e330a54445a6cbf06d6f614985ed
acongg2 <calvincong67@gmail.com>
2022-12-01 21:10:17 +0700

    update BE
    API WORKED :D

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
-import json
+import bson.json_util as json_util
 
 app = Flask(__name__)
-client = MongoClient("mongodb+srv://admin:admin@lasti.gi8e0eg.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://admin:admin@lasti.gi8e0eg.mongodb.net/Samsat_Keliling")
 db = client["BAPENDA"]
+
+def parse_json(data):
+    return json_util.dumps(data)
 
 @app.route('/Samling', methods=["GET"])
 def get_data():
-    data = client["BAPENDA"]["Samsat Keliling"]
-    records = data.find({})
-    json_obj = json.dumps(records)
-    return jsonify(json_obj)
+    collection = client["BAPENDA"]["Samsat_Keliling"]
+    datas = []
+    for data in collection.find({}):
+        datas.append(data)
+    return parse_json(datas)
     client.close()
 
 
